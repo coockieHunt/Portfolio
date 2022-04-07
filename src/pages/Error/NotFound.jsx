b3581e187bdb3f6de0f5039687223e23293641df
cookieHunt <jonathan.gleyze@gmail.com>
2022-04-07 22:51:22 +0200

    add teapot wip

--- src/pages/Error/NotFound.jsx
+++ src/pages/Error/NotFound.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import{
+    Teapot
+} from '../../utils/style/page/error/style'
+
+import TeapotLogo from '../../utils/img/other/teapot.svg'
 
 
 const Error = styled.div`
@@ -10,7 +15,12 @@ const Error = styled.div`
 
 function NotFound(){
     return(
+        <div>
+     <Teapot src={TeapotLogo} alt="Node Js logo" />
+        
         <Error>server : Ooops 404 not found<br/><br/>me :can you excuse me ? 😟<br/> you: YES 💚 <br/> me : Thank you I swear it won't happen again !! 😊😎</Error>
+        </div>
+   
     )
 }
 
---
+++ src/utils/style/page/error/style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import colors from "../../library/color";
+
+
+export const Teapot = styled.img `
+    height: 350px;
+    fill: red;
+    filter: ${colors.primary_filter};
+    float: left;
+`;
