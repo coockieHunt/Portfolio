adf6ac44de96ac211aead293678c2fada302d3df
coockieHunt <jonathan.gleyze@gmail.com>
2022-06-05 20:00:03 +0200

    add svg to skill section

--- src/utils/style/library/size.jsx
+++ src/utils/style/library/size.jsx
@@ -5,10 +5,25 @@ export const size_Width = {
     desktop: '2560px'
 }
 
+export const size_Height = {
+    mobile: '667px',
+    tablet: '1280px',
+    laptop: '1440px',
+    desktop: '2560px'
+}
+
 
 export const device = {
     mobileM: `(max-width: ${size_Width.mobile})`,
     tablet: `(max-width: ${size_Width.tablet})`,
     laptop: `(max-width: ${size_Width.laptop})`,
     desktop: `(max-width: ${size_Width.desktop})`,
+};
+
+
+export const device_Height = {
+    mobileM: `(max-width: ${size_Height.mobile})`,
+    tablet: `(max-width: ${size_Height.tablet})`,
+    laptop: `(max-width: ${size_Height.laptop})`,
+    desktop: `(max-width: ${size_Height.desktop})`,
 };
